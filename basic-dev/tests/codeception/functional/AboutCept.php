8ce785a68fdab85cc9b8cf82cb4cdac1a0124d23
Matteo 'Peach' Pescarin <peach@smartart.it>
2014-11-23 16:05:52 +0000

    updated About and Home CEPT tests, made them a bit more relevant

--- basic-dev/tests/codeception/functional/AboutCept.php
+++ basic-dev/tests/codeception/functional/AboutCept.php
@@ -5,4 +5,8 @@ use tests\codeception\_pages\AboutPage;
 $I = new FunctionalTester($scenario);
 $I->wantTo('ensure that about works');
 AboutPage::openBy($I);
+$I->seeInTitle('About');
 $I->see('About', 'h1');
+
+$I->expectTo('see the link of to the about page as selected');
+$I->seeElement('//li[@class="active"]/a[contains(.,"About")]');
--- basic-dev/tests/codeception/functional/HomeCept.php
+++ basic-dev/tests/codeception/functional/HomeCept.php
@@ -3,7 +3,22 @@
 $I = new FunctionalTester($scenario);
 $I->wantTo('ensure that home page works');
 $I->amOnPage(Yii::$app->homeUrl);
-$I->see('My Company');
-$I->seeLink('About');
-$I->click('About');
-$I->see('This is the About page.');
+
+$I->expect('the title to be set correctly');
+$I->seeInTitle('My Yii Application');
+
+$I->expectTo('see all the links of the menu');
+$url = $I->grabFromCurrentUrl();
+$I->seeLink('Home', $url);
+$I->seeLink('About', '/about');
+$I->seeLink('Login', '/login');
+$I->seeLink('Contact', '/contact');
+$I->dontSeeLink('About', 'site/about');
+$I->dontSeeLink('Login', 'site/login');
+$I->dontSeeLink('About', 'site/contact');
+
+$I->expectTo('see a self-referencing link to my company homepage');
+$I->seeLink('My Company', $url);
+
+$I->expectTo('see the link of the homepage as selected');
+$I->seeElement('//li[@class="active"]/a[contains(.,"Home")]');
