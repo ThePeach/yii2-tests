600c1fc5781b7fa3e0670a84d00de424a77928e2
Matteo 'Peach' Pescarin <peach@smartart.it>
2014-12-20 15:46:01 +0000

    added code to authenticate with the application via REST
    - updated AppAsset bundle
    - added close window to modal
    - removed client validation from form
    - added error message to modal
    - added js code to do everything needed

--- basic-dev/views/site/login-modal.php
+++ basic-dev/views/site/login-modal.php
@@ -5,18 +5,21 @@ use yii\widgets\ActiveForm;
 /* @var $this yii\web\View */
 /* @var $form yii\widgets\ActiveForm */
 /* @var $model app\models\LoginForm */
-
-$this->title = 'Login';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
 
     <p>Please fill out the following fields to login:</p>
+
+    <div class="alert alert-danger alert-dismissible fade in">
+        <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+        <p class="error"></p>
+    </div>
 
     <?php $form = ActiveForm::begin([
         'id' => 'login-form',
         'options' => ['class' => 'form-horizontal'],
+        'enableClientScript' => false,
+        'enableClientValidation' => false,
         'fieldConfig' => [
             'template' => "{label}\n<div class=\"col-lg-4\">{input}</div>\n<div class=\"col-lg-5\">{error}</div>",
             'labelOptions' => ['class' => 'col-lg-offset-1 col-lg-2 control-label'],
@@ -26,10 +29,6 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= $form->field($model, 'username') ?>
 
     <?= $form->field($model, 'password')->passwordInput() ?>
-
-    <?= $form->field($model, 'rememberMe', [
-        'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-    ])->checkbox() ?>
 
     <div class="form-group">
         <div class="col-lg-offset-1 col-lg-11">
