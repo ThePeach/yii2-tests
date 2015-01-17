90732acfdbb88b9d3b1a9060f91d54dcc4f08675
Matteo 'Peach' Pescarin <peach@smartart.it>
2015-01-17 17:49:33 +0000

    updated codeception configuration
    - small amend on index-test inclusion of c3

--- basic-dev/web/index-test.php
+++ basic-dev/web/index-test.php
@@ -11,7 +11,8 @@ defined('YII_ENV') or define('YII_ENV', 'test');
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
+include_once __DIR__ . '/c3.php';
+
 $config = require(__DIR__ . '/../tests/codeception/config/acceptance.php');
 
-include 'c3.php';
 (new yii\web\Application($config))->run();
